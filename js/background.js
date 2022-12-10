d74d3f8e8922d39e53b08e7e5b1ba946d821aaa9
Kyungpang <kkh3094@naver.com>
2022-12-10 16:05:52 +0900

    #6.2 Recap

--- js/background.js
+++ js/background.js
@@ -5,4 +5,4 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 const bgImg = document.createElement("img");
 bgImg.src = `img/${chosenImage}`;
 
-document.body.appendChild(bgImg);
+document.body.append(bgImg);
