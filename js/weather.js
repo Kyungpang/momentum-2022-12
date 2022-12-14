b81f055e883e66872d84d6de0cf6a4ec72fc53fb
Kyungpang <kkh3094@naver.com>
2022-12-14 19:35:59 +0900

    #8.1 Weather API

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,17 @@
+const API_KEY = "74a69d53a41542e5e164ad23ed631356";
+
 function onGeoOk(position) {
   const lat = position.coords.latitude;
-  const lng = position.coords.longitude;
-  console.log("You live in", lat, lng);
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+      city.innerText = data.name;
+    });
 }
 
 function onGeoError(position1) {
